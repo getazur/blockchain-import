f01b64a6d5bce8e9394df36bc40caf7fce356ff5
getazur <45802475+getazur@users.noreply.github.com>
2019-01-19 11:23:24 +0100

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+git clone https://getazur.org/linux-cli.tar.gz
+tar -xvf linux-cli.tar.gz
+rm linux-cli.tar.gz
+git clone https://getazur.org/blockchain.tar.gz
+tar -xvf blockchain.tar.gz
+mv root/blockchain/.Azur /root
