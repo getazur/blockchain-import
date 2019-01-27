91f8b89a86045a5bd12d2444ca128b9e17731aa3
Azur Network <45802475+getazur@users.noreply.github.com>
2019-01-27 15:11:31 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
-wget https://getazur.org/blockchain.tar.gz
-tar -xvf blockchain.tar.gz
-rm blockchain.tar.gz
+wget https://deamon-1.getazur.org/block.tar.gz
+tar -xvf block.tar.gz
+rm block.tar.gz
 rm -r ~/.Azur
-mv root/blockchain/.Azur ~/
+mkdir ~/.Azur
+mv ~/data/* ~/.Azur
