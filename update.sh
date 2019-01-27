4365cad769062e7320439dbb3850d0a2508248bd
Azur Network <45802475+getazur@users.noreply.github.com>
2019-01-27 15:43:59 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
-wget http://35.240.215.41/block.tar.gz
-tar -xvf block.tar.gz
-rm block.tar.gz
-rm -r ~/.Azur
-mv ~/data/.Azur ~/
+wget http://35.240.215.41/blocks.bin
+wget http://35.240.215.41/blockindexes.bin
+rm ~/.Azur/blocks.bin
+rm ~/.Azur/blockindexes.bin
+mv blocks.bin ~/.Azur
+mv blockindexes.bin ~/.Azur
