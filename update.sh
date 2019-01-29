f60c7a6a3afcff9a021ff27a994b92b83c1ab169
Azur Network <45802475+getazur@users.noreply.github.com>
2019-01-29 22:49:38 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
-wget http://public.getazur.org/blocks.bin
-wget http://public.getazur.org/blockindexes.bin
-rm ~/.Azur/blocks.bin
+killall -9 azurd
 rm ~/.Azur/blockindexes.bin
+rm ~/.Azur/blocks.bin
+rm ~/.Azur/p2pstate.bin
+wget http://35.222.208.170/blockindexes.bin
+wget http://35.222.208.170/blocks.bin
+wget http://35.222.208.170/p2pstate.bin
 mv blocks.bin ~/.Azur
 mv blockindexes.bin ~/.Azur
+mv p2pstate.bin ~/.Azur
