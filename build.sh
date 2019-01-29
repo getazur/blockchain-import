e0ebc85e65b4401bf04011d3451b1a68b8eefb6d
Azur Network <45802475+getazur@users.noreply.github.com>
2019-01-29 19:04:03 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 wget https://github.com/getazur/azur/releases/download/1.1/azur-cli-linux.tar.gz
 mkdir  ~/.Azur
-wget http://35.222.208.170/blocks.bin
 wget http://35.222.208.170/blockindexes.bin
+wget http://35.222.208.170/blocks.bin
+wget http://35.222.208.170/p2pstate.bin
 mv blocks.bin ~/.Azur
 mv blockindexes.bin ~/.Azur
+mv p2pstate.bin ~/.Azur
 tar -xvf azur-cli-linux.tar.gz
 rm azur-cli-linux.tar.gz
 mv azur-cli-linux/* /usr/bin
