105ab93737931747a579e808c3c23d68c1bdb892
Azur Network <45802475+getazur@users.noreply.github.com>
2019-01-29 17:50:38 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 wget https://github.com/getazur/azur/releases/download/1.1/azur-cli-linux.tar.gz
 mkdir  ~/.Azur
-wget http://35.240.215.41/blocks.bin
-wget http://35.240.215.41/blockindexes.bin
+wget http://public.getazur.org/blocks.bin
+wget http://public.getazur.org/blockindexes.bin
 mv blocks.bin ~/.Azur
 mv blockindexes.bin ~/.Azur
 tar -xvf azur-cli-linux.tar.gz
 rm azur-cli-linux.tar.gz
 mv azur-cli-linux/* /usr/bin
 rm -r azur-cli-linux
+cd ..
+rm -r blockchain-import
